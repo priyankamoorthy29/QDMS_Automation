1ca0c73f8f2ba498adfa18c59d13bfaa7a8ad39a
sasikala-as <you@example.com>
2023-01-30 13:17:08 +0530

    my commit

--- src/test/java/Pages/CustomerPage.java
+++ src/test/java/Pages/CustomerPage.java
@@ -5,11 +5,27 @@ import org.openqa.selenium.support.FindBy;
 
 public class CustomerPage {
 	
-	@FindBy (xpath = "//*[@id=\"root\"]/div/section/section/main/div/div/div[2]/div/div/div/div/div[1]/div/div/div[6]/div/button/span")
-	public static WebElement addcustomer;
-	@FindBy(xpath = "//*[@id=\"root\"]/div/section/section/header/ul/li[4]/span/a")
-	public static  WebElement   customerbar ; 
-	@FindBy(xpath = "//*[@id=\"root\"]/div/section/section/main/div/div/div[1]/div/div[1]/div[1]")
+	@FindBy(xpath="//*[@id ='usernameOrEmail']")
+    public static WebElement username;
+    
+    @FindBy(id="password")
+    public static WebElement password;
+    
+    @FindBy(xpath= "//button[@type]")
+    public static WebElement login;
+    
+    @FindBy (xpath = "//a[@class='sc-bdVaJa ieeiNp'][@href=\"#/master/plantlevel\"]")
+    public static WebElement mastercard;
+    
+    @FindBy (xpath = "//*[@id=\"root\"]/div/section/section/header/ul/li[4]/span/a")
+    public static WebElement customerbar;
+    
+    @FindBy (xpath = "//*[@id=\"root\"]/div/section/section/main/div/div/div[1]/div/div[1]/div[1]")
     public static WebElement customersitebutton;
+    
 	
+	@FindBy (xpath = "//*[@id=\"root\"]/div/section/section/main/div/div/div[2]/div/div/div/div/div[1]/div/div/div[6]/div/button/span")
+	public static WebElement AddcustomerButton;
+			
+			
 }
--- src/test/java/TestCases/CustomerTest.java
+++ src/test/java/TestCases/CustomerTest.java
@@ -1,107 +1,238 @@
 package TestCases;
 
+
 import java.io.IOException;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import Base.DriverInitialization;
-import Pages.CustomerPage;
-import Pages.LoginPage;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
-public class CustomerTest extends DriverInitialization {
-	
-		
-		CustomerPage custbtn = new CustomerPage();
-		
-		@Test
-		    public void adminButton() throws InterruptedException, IOException{
-		            
-		        
-		    PageFactory.initElements(driver,custbtn);
-		    
-		  
-		    
+import Base.DriverInitialization;
+
+
+
+
+public class CustomerTest extends DriverInitialization{
+
+	Pages.CustomerPage CustomerPage = new Pages.CustomerPage();
+	@Test
+		public void AddcustomerButtoncheck() throws InterruptedException , IOException
+		{
+			
+			PageFactory.initElements(driver, CustomerPage);
+			
+			CustomerPage.username.sendKeys("admin");
+		    CustomerPage.password.sendKeys("tokyo@admin");
+		    CustomerPage.login.click();
 		    Thread.sleep(2000);
-			CustomerPage.customerbar.click();
-			
-			
+		    CustomerPage.mastercard.click();
+		    Thread.sleep(2000);
+		    CustomerPage.customerbar.click();
 			Thread.sleep(2000);
 			CustomerPage.customersitebutton.click();
 			Thread.sleep(2000);
-			CustomerPage.addcustomer.click();
-		    //adminButton.CheckAdmin.click();
+			
+		//	SoftAssert sa = new SoftAssert();
 
-		    
-		    
-		    SoftAssert aa = new SoftAssert();
-		    Point actual = CustomerPage.addcustomer.getLocation();
-		    
-		    int x = actual.getX();
-		    int expectedresult = 970;
-		    System.out.println("x-axis = " + x);
-		    aa.assertEquals(x, expectedresult);
-		    
-		    
-		    int y = actual.getY();
-		    int expectresult =  97;
-		    System.out.println("y-axis = " + y);
-		    aa.assertEquals(expectresult, y);
-		    
-		    
-		    org.openqa.selenium.Dimension size = CustomerPage.addcustomer.getSize();
-		    int width = size.getWidth();
-		    System.out.println("width  = " + width);
-		    int expectedresult1 =89;
-		    aa.assertEquals(width, expectedresult1);    
-		     
-		    int height = size.getHeight();
-		    System.out.println("height = " + height);
-		    int expectedresult2 =22;
-		    aa.assertEquals(height, expectedresult2);
-		    
-		    
-		    String backgroundColor = CustomerPage.addcustomer.getCssValue("background-color");
-		    System.out.println("backgroundclr = " + backgroundColor );
-		    String bgc = "rgba(0, 0, 0, 0)";
-		    aa.assertEquals(backgroundColor, bgc);
-		    
-		    
-		    
-		    
-		    String borderColor = CustomerPage.addcustomer.getCssValue("border-color");
-		    System.out.println("borderColor = " + borderColor);
-		    String bc = "rgb(255, 255, 255)";
-		    aa.assertEquals(borderColor, bc);
-		    
-
-		    
-		    
-
-		    String font = CustomerPage.addcustomer.getCssValue("font-family");
-		    System.out.println("Font of button: " + font);
-		    String ft = "Roboto, sans-serif";
-		    aa.assertEquals(font, ft);
-		    
-		    
-		    
-		    String color = CustomerPage.addcustomer.getCssValue("color");
-		    System.out.println("Font color of button: " + color);
-		    String fontclr = "rgba(255, 255, 255, 1)";
-		    aa.assertEquals(fontclr, color);
-		    
-		    
-		     String txt = CustomerPage.addcustomer.getText();
-		     System.out.println("text = " + txt);
-		     String txxt = "Add Customer";
-		     aa.assertEquals(txt, txxt);
-		     
-		     aa.assertAll();
-		    	    
+			WebElement AddcustomerButton = driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/section/main/div/div/div[2]/div/div/div/div/div[1]/div/div/div[6]/div/button/span"));
+			
+		//	ExtentHtmlReporter reporter = new ExtentHtmlReporter("./Reports/extentreport.html");
+			
+		//	ExtentReports extent = new ExtentReports();
+		
+		//	extent.attachReporter(reporter);
+			
+			
+			
+			
+			 //check if element visible
+			
+			boolean AcctualElement=AddcustomerButton.isDisplayed();
+	        boolean ExpectedElement=true;
+	        
+	        
+	        
+		
+	        boolean elementvisible = true;
+	        testCase = extent.createTest("ELEMENT-VISIBLE");
+			try {
+				AssertJUnit.assertEquals(AcctualElement,ExpectedElement);
+			} catch (AssertionError e) {
+				elementvisible=false;
+			}
+			if (elementvisible) {
+				testCase.log(Status.INFO,"ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO,"ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO,"Element Available");
+				testCase.log(Status.PASS,"Correct element");
+			} else {
+				testCase.log(Status.INFO,"ActualElement :- " + AcctualElement);
+				testCase.log(Status.INFO,"ExpectedElement :- " + ExpectedElement);
+				testCase.log(Status.INFO,"Element not available");
+				testCase.log(Status.FAIL,"No Element");
+			}
+				Thread.sleep(2000);	
+			
+				
+				
+			//Check position
+			
+			Point ActualLocation = AddcustomerButton.getLocation();
+	        
+	        int actual_x = ActualLocation.getX();
+	        int actual_y = ActualLocation.getY();
+			
+			System.out.println("X axis: "+actual_x);
+			System.out.println("Y axis: "+actual_y);
+			
+			
+			
+			Point ExpectedLocation = new Point(1229,98);
+			
+			
+			 boolean position = true;
+			 testCase = extent.createTest("POSITION");
+				try {
+					AssertJUnit.assertEquals(ActualLocation,ExpectedLocation);
+				} catch (AssertionError e) {
+					position=false;
+				}
+				if (position) {
+					testCase.log(Status.INFO,"ActualLocation :- " + ActualLocation);
+					testCase.log(Status.INFO,"ExpectedLocation :- " + ExpectedLocation);
+					testCase.log(Status.INFO,"Correct Position");
+					testCase.log(Status.PASS,"Position correct");
+				} else {
+					testCase.log(Status.INFO,"ActualLocation :- " + ActualLocation);
+					testCase.log(Status.INFO,"ExpectedLocation :- " + ExpectedLocation);
+					testCase.log(Status.INFO,"wrong Position");
+					testCase.log(Status.FAIL,"Position Wrong");
+				}
+					Thread.sleep(2000);	
+					
+					
+					
+			// check font size
+			
+					String ActualfontsSize = AddcustomerButton.getCssValue("font-size"); 
+					System.out.println("Font Size: "+ActualfontsSize); 
+					testCase = extent.createTest("FONT-SIZE");
+					String ExpectedfontsSize ="14px";
+					boolean fontsize = true;
+					try {
+						AssertJUnit.assertEquals(ActualfontsSize, ExpectedfontsSize);
+					} catch (AssertionError e) {
+						fontsize=false;
+					}
+					if (fontsize) {
+						testCase.log(Status.INFO,"ActualfontsSize :- " + ActualfontsSize);
+						testCase.log(Status.INFO,"ExpectedfontsSize :- " + ExpectedfontsSize);
+						testCase.log(Status.INFO,"Correct font-size");
+						testCase.log(Status.PASS,"Font-size Correct");
+					} else {
+						testCase.log(Status.INFO,"ActualfontsSize :- " + ActualfontsSize);
+						testCase.log(Status.INFO,"ExpectedfontsSize :- " + ExpectedfontsSize);
+						testCase.log(Status.INFO,"Wrong font-size");
+						testCase.log(Status.FAIL,"Font-size Wrong");
+					}
+						Thread.sleep(2000);	
+					 
+					
+			// Check the size
+					
+					Dimension ActualSize = AddcustomerButton.getSize();
+					System.out.println("Size :"+ActualSize);
+					Dimension ExpectedSize = new Dimension(89,22);
+					
+					
+					boolean size = true;
+					testCase = extent.createTest("BUTTON-SIZE");
+					try {
+						AssertJUnit.assertEquals(ActualSize,ExpectedSize);
+					} catch (AssertionError e) {
+						size=false;
+					}
+					if (size) {
+						testCase.log(Status.INFO,"ActualSize :- " + ActualSize);
+						testCase.log(Status.INFO,"ExpectedSize :- " + ExpectedSize);
+						testCase.log(Status.INFO,"Correct Size");
+						testCase.log(Status.PASS,"Correct Size");
+					} else {
+						testCase.log(Status.INFO,"ActualSize :- " + ActualSize);
+						testCase.log(Status.INFO,"ExpectedSize :- " + ExpectedSize);
+						testCase.log(Status.INFO,"wrong Size");
+						testCase.log(Status.FAIL,"wrong Size");
+					}
+						Thread.sleep(2000);	
+					
+					
+			//Check test button color
+					
+					String ActualColour = AddcustomerButton.getCssValue("background-color");
+					System.out.println("rgb :"+ActualColour);
+					
+					
+					//String hexcolour = Color.fromString(ActualColour).asHex();
+					String ExpectedColour = "rgba(0, 0, 0, 0)";
+			
+			
+					
+					
+					boolean colour = true;
+					
+					testCase = extent.createTest("BUTTON-COLOUR");
+					try {
+						AssertJUnit.assertEquals(ActualColour,ExpectedColour);
+					} catch (AssertionError e) {
+						colour=false;
+					}
+					if (colour) {
+						testCase.log(Status.INFO,"ActualColour :- " + ActualColour);
+						testCase.log(Status.INFO,"ExpectedColour :- " + ExpectedColour);
+						testCase.log(Status.INFO,"Correct Colour");
+						testCase.log(Status.PASS,"Correct Colour");
+					} else {
+						testCase.log(Status.INFO,"ActualColour :- " + ActualColour);
+						testCase.log(Status.INFO,"ExpectedColour :- " + ExpectedColour);
+						testCase.log(Status.INFO,"wrong Colour");
+						testCase.log(Status.FAIL,"wrong Colour");
+					}
+						Thread.sleep(2000);	
+					
+					
+					//sa.assertAll();
+//			if(Actual==Expected)
+//			{
+//				System.out.println("Correct colour");
+//			}
+//			else
+//			{
+//				System.out.println("Wrong colour");
+//			}
+			
+//			
+//					ExtentTest logger = extent.createTest("Login Test");
+//					logger.log(Status.INFO,"Login to HRM");
+//					logger.log(Status.PASS,"Login Verified");
+//					extent.flush();
+//			
+			
+						
 		}
-		    
+		
+		
 	}
 
+	
+	
+	
